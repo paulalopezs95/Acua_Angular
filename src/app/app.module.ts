b4584f56bdacddfc6d923cd7404216b75cc0f259
paulalopezs95 <paula.lopezs95@gmail.com>
2020-10-13 09:00:24 -0500

    Proyecto Acua Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { InfoComponent } from './info/info.component';
+import { MenuComponent } from './oficinaVirtual/menu/menu.component';
+import { TicketsComponent } from './oficinaVirtual/tickets/tickets.component';
+import { PedidosComponent } from './oficinaVirtual/pedidos/pedidos.component';
+import { MisDatosComponent } from './oficinaVirtual/mis-datos/mis-datos.component';
+import { LoginComponent } from './acceso/login/login.component';
+import { RegistroComponent } from './acceso/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    InfoComponent,
+    FooterComponent,
+    MenuComponent,
+    TicketsComponent,
+    PedidosComponent,
+    MisDatosComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
